e44f8827e08b5a58be472e0805f73c6950184fb1
Sebastian Miranda <35125339+SMG-1@users.noreply.github.com>
2022-06-03 08:25:53 -0600

    added snowflake query to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
+from httpcore import stream
 import streamlit
 import pandas as pd
 import requests
+import snowflake.connector
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -28,3 +30,11 @@ fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_c
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+# query snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute('SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()')
+my_data_row = my_cur.fetchone()
+streamlit.text('Hello from Snowflake:')
+streamlit.text(my_data_row)
